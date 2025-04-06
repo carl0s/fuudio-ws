4e4268265ac4f15b8e6918131a5db6184faa8d96
Carlo Frinolli <carlo.frinolli@gmail.com>
2025-04-06 18:09:44 +0200

    updated

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true, // Abilita React Strict Mode per evidenziare potenziali problemi nell'app
   swcMinify: true,      // Abilita la minificazione SWC per build più veloci
+  output: 'standalone', // Aggiunge l'output standalone per Docker
 
   // Aggiungi qui altre configurazioni di Next.js se necessario
   // Esempio:
